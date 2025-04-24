d8c337a362fb61f6b280a6ae6e0fc032d0ae5d24
Bishoy Samwel <bishoy.samwuel@gmail.com>
2025-04-24 14:10:49 +0200

    feat: add MySQL connection setup using mysql2 client

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+require('dotenv').config();
+
+// Import connections
+const db = require('./config/mysql');
+// const redisClient = require('./config/redis');
+
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -9,4 +15,3 @@ app.use('/', routes);
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
-
