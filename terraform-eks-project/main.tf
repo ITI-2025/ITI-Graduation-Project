25cafc90291e83b7c1cfff823fa8f66b623968a3
ahlambakr <ahlamelghrib2020@gmail.com>
2025-04-26 15:37:47 +0300

    cluster-communicate-with-basion

--- terraform-eks-project/main.tf
+++ terraform-eks-project/main.tf
@@ -5,6 +5,31 @@ module "vpc" {
   private_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 }
 
+
+
+resource "aws_security_group" "eks_api_sg" {
+  name        = "eks-api-sg"
+  description = "Allow Bastion Host to access EKS API"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["${module.ec2_instance.private_ip}/32"]  
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
+
+  tags = {
+    Name = "eks-api-sg"
+  }
+}
 module "my-eks" {
   source  = "terraform-aws-modules/eks/aws"
   # version = "~> 20.31"
@@ -16,21 +41,7 @@ module "my-eks" {
   subnet_ids = module.vpc.private_subnet_ids
   cluster_endpoint_private_access = true
   cluster_endpoint_public_access  = true
-  # public_access_cidrs = ["${module.bastion.private_ip}/32"]
-
-  #   cluster_security_group_additional_ingress_rules = [
-  #   {
-  #     description              = "Allow Bastion Host to access EKS API"
-  #     from_port                = 443
-  #     to_port                  = 443
-  #     protocol                 = "tcp"
-  #     cidr_blocks              = ["${module.bastion.private_ip}/32"]
-  #     ipv6_cidr_blocks         = []
-  #     prefix_list_ids          = []
-  #     security_groups          = []
-  #     self                     = false
-  #   }
-  # ]
+    cluster_security_group_id = aws_security_group.eks_api_sg.id
 
   tags = {
     Name = "my-eks-cluster"
@@ -57,10 +68,8 @@ module "ec2_instance" {
   ami_id = "ami-0ff8a91507f77f867"  
   instance_type     = "t2.micro"
   subnet_id         = module.vpc.public_subnet_ids[0]
-   vpc_id           = module.vpc.vpc_id
+  vpc_id           = module.vpc.vpc_id
   key_name          = "basion-key-2"
   user_data         = file("user-data.sh")
   name              = "my-basion-host"
- 
-  
 }
