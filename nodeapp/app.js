e2be6cdf60ae8bc6e0a992a975779b6fef3fc676
AhmedAbo3li <dev.ahmed3li@outlook.com>
2025-04-28 11:23:31 +0300

    some editing

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -91,7 +91,7 @@ app.get('/', async (req, res) => {
       // Cache the new count in Redis for 10 seconds
       await redisClient.setEx('visit_count', 10, count.toString());
 
-      res.send(`Hello World another world from ITI 44! Page visits: ${count}`);
+      res.send(`Hello cutie ! Page visits: ${count}`);
     } finally {
       await connection.end();
     }
