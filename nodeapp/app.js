e73726d0c3e5b146195dc94bbc0e5b055c4e96bf
AhmedAbo3li <dev.ahmed3li@outlook.com>
2025-05-02 23:49:42 +0300

    test for the demoo

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -91,7 +91,7 @@ app.get('/', async (req, res) => {
       // Cache the new count in Redis for 10 seconds
       await redisClient.setEx('visit_count', 10, count.toString());
 
-      res.send(`Hello World Weclome To Our Project Demo ! Page visits: ${count}`);
+      res.send(`Test For The Demo! Page visits: ${count}`);
     } finally {
       await connection.end();
     }
