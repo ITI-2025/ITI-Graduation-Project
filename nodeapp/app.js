135068f798997a9bc425c172f7434a2731e7fef6
AhmedAbo3li <dev.ahmed3li@outlook.com>
2025-05-02 09:39:28 +0300

    dkajfdhajhfa

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -91,7 +91,7 @@ app.get('/', async (req, res) => {
       // Cache the new count in Redis for 10 seconds
       await redisClient.setEx('visit_count', 10, count.toString());
 
-      res.send(`Hello Final Round! Page visits: ${count}`);
+      res.send(`Hello Test Final Rounddddd b2aaaaaa! Page visits: ${count}`);
     } finally {
       await connection.end();
     }
